9ffdac41b88a1a6a92caeeb0f91eb255d2ef4180
teledima <teledima00@gmail.com>
2021-07-05 22:24:25 +0500

    Output to the first question page

--- tests/models.py
+++ tests/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class TestInfo(models.Model):
@@ -9,8 +10,52 @@ class TestInfo(models.Model):
     creator = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True,
                                 verbose_name='Составитель теста', to_field='username')
     time_to_solve = models.PositiveIntegerField(verbose_name='Время для решения', null=True)
+    questions = models.ManyToManyField(to='QuestionInfo', through='TestQuestion')
 
     class Meta:
         db_table = 'tests_info'
         verbose_name = 'Test Info'
         verbose_name_plural = 'Tests Info'
+
+
+class QuestionInfo(models.Model):
+    id = models.AutoField(primary_key=True)
+    question_text = models.CharField(max_length=4000, verbose_name='Текст вопроса')
+    question_type = models.ForeignKey(to='QuestionType', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'questions_info'
+        verbose_name = 'Question info'
+        verbose_name_plural = 'Questions info'
+
+
+class QuestionType(models.Model):
+    id = models.AutoField(primary_key=True)
+    type_name = models.CharField(max_length=100, verbose_name=_('Type name'))
+    html_block = models.CharField(max_length=4000, verbose_name=_('Html code block for type'), default='')
+
+    class Meta:
+        db_table = 'question_types'
+        verbose_name = 'Question Type'
+        verbose_name_plural = 'Question types'
+
+
+class TestQuestion(models.Model):
+    test = models.ForeignKey(to=TestInfo, on_delete=models.CASCADE)
+    question = models.ForeignKey(to=QuestionInfo, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'tests_questions'
+        verbose_name = 'Question in test'
+        verbose_name_plural = 'Questions in test'
+
+
+class QuestionAnswer(models.Model):
+    question = models.ForeignKey(to=QuestionInfo, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=255, verbose_name='Вариант ответа')
+    is_correct = models.PositiveIntegerField(verbose_name='Правильность ответа')
+
+    class Meta:
+        db_table = 'questions_answers'
+        verbose_name = 'Answer'
+        verbose_name_plural = 'Answers'
--- tests/views.py
+++ tests/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.core import serializers
 from tests.models import TestInfo
 
@@ -6,10 +6,16 @@ from tests.models import TestInfo
 def start_page(request):
     context = {'all_tests': serializers.serialize('python', TestInfo.objects.all()),
                'columns': [field.verbose_name for field in TestInfo._meta.get_fields()
-                           if field.verbose_name != ''],
+                           if hasattr(field, 'verbose_name') and field.verbose_name != ''],
                'test_string': 'test'}
     return render(request, 'tests/start_page.html', context=context)
 
 
 def start_test(request):
-    return render(request, 'tests/test.html', context={'id': request.POST.get('test-id')})
+    first_question = TestInfo(request.POST.get('test-id')).questions.first()
+    answers = [first_question.question_type.html_block.format(name=first_question.question_type.type_name,
+                                                              id=answer.id,
+                                                              text=answer.answer_text)
+               for answer
+               in first_question.questionanswer_set.all()]
+    return render(request, 'tests/test.html', context={'question': first_question, 'answers': answers})
