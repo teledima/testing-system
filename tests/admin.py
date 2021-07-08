ded1c0fe3237460db6db40ce1ef570d6c235f678
teledima <46664596+teledima@users.noreply.github.com>
2021-07-09 01:33:37 +0500

    При нажатии на кнопку "Начать тестирование" запустить тестирование по выбранному тесту (#2)
    * Go to test page on button click. Also:
    - when you click on a line, the selected test is highlighted. Only one test can be selected
    
    * Output to the first question page
    
    * Create timer

--- tests/admin.py
+++ tests/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import TestInfo
+from .models import TestInfo, TestQuestion, QuestionAnswer, QuestionInfo
 
 admin.site.register(TestInfo)
+admin.site.register(TestQuestion)
+admin.site.register(QuestionAnswer)
+admin.site.register(QuestionInfo)
---
+++ tests/static/tests/main_page.js
@@ -0,0 +1,22 @@
+$('#all-tests tr:not(.table-head)').on('click', function(e) {
+	if ($(this).hasClass('selected'))
+	{
+	    $(this).removeClass('selected');
+	    $("input[type='submit']").prop('disabled', true);
+	}
+	else
+	{
+		$('#all-tests tr.selected').removeClass('selected');
+		$(this).addClass('selected');
+		$("input[type='submit']").prop('disabled', false);
+	}
+});
+
+
+$('#start-testing').on('submit', function(e) {
+    $('<input>').attr({
+        type: 'hidden',
+        name: 'test-id',
+        value: $('#all-tests tr.selected').data('id')
+    }).appendTo('#start-testing');
+});
